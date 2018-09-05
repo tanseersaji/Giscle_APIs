53f278662f44a15a955ace7e28d85351b09de90a
Suman Kumar Jha <17616710+jhasuman@users.noreply.github.com>
2018-09-06 00:32:05 +0530

    Add files via upload

---
+++ Img_client_Face.py
@@ -0,0 +1,44 @@
+import base64
+import requests
+import time,cv2
+
+g_url = 'http://api.giscle.ml'
+
+
+img_path = 'input/test.jpg'
+frame = cv2.imread(img_path)
+
+img = open(img_path, "rb").read()
+img = base64.b64encode(img)
+
+token = "Paste Your Token"
+
+payload = {'image': img }
+
+r = requests.post(g_url + ':80/image', files=payload, headers={'token': token})
+
+if r.ok:
+    #print(r.json())
+    preds =r.json()
+    print("{}".format(preds['Data']))
+    while True:
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        if len(preds.keys()) and preds['Data'][1] != 0:
+            #print(preds['Data'])
+            for key in preds['Data'][2].keys():
+                    gender = preds['Data'][2][str(key)]['Gender']
+                    x,y,h,w = preds['Data'][2][str(key)]['rect_coordinate']
+                    age = preds['Data'][2][str(key)]['Age']
+                    emotion = preds['Data'][2][str(key)]['Emotion']
+                    #accuracy = preds['Output'][2][str(key)]['Accuracy']
+                    #cv2.putText(frame,'Gender : ' + gender,(x,y-25), font, 0.5, (200,255,155), 1, cv2.LINE_AA)
+                    #cv2.putText(frame,'Age : ' + str(age),(x,y-50), font, 0.5, (200,255,155), 1, cv2.LINE_AA)
+                    #cv2.putText(frame,'Emotion : ' + str(emotion),(x,y-75), font, 0.5, (200,255,155), 1, cv2.LINE_AA)
+                    cv2.putText(frame,'Total_faces : {}'.format(preds['Data'][1]),(10,20), font, 0.5, (200,255,155), 1, cv2.LINE_AA)
+                    cv2.rectangle(frame, (x,y),(x+h,y+w), (255,255,255))
+    
+        cv2.imshow("frame",frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+else:
+    print('Status: {}'.format(r.status_code))
---
+++ vid_client_Body_detection.py
@@ -0,0 +1,45 @@
+from socketIO_client import SocketIO, LoggingNamespace
+import cv2
+import hashlib
+import base64
+import time
+
+token = 'Paste Your Token'
+
+
+g_url = 'http://api.giscle.ml'
+
+
+def extract_data(args):
+    
+    print(args)
+
+socketio = SocketIO(g_url, 80, LoggingNamespace)
+
+socketio.emit('authenticate', {'token': token})
+
+cam = cv2.VideoCapture(0)
+
+frame_count = 1
+
+while True:
+    global t
+    t = time.clock()
+    ret, frame = cam.read()
+    if not ret:
+        continue
+    frame = cv2.resize(frame, (900, 600))
+    encoded, buffer = cv2.imencode('.jpg', frame)
+    encoded_frame = base64.b64encode(buffer)
+    encoded_frame = encoded_frame.decode('utf-8')
+    socketio.emit('count_people', {'data': encoded_frame})
+    socketio.on('response', extract_data)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.imshow("frame",frame)
+    socketio.wait(0.0001)
+    print(time.clock() - t)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+socketio.disconnect()
+cam.release()
